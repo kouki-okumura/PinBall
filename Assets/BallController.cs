ded913787cd235f08308ce4be02be988d27f0577
奥村公基 <okumura0329k@gmail.com>
2018-04-27 22:08:34 +0900

    Lesson5 Pinball　課題提出
    発展課題もあります

---
+++ Assets/BallController.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class BallController : MonoBehaviour {
+    private int score = 0;
+    private float visiblePosZ = -6.5f;
+    private GameObject gameoverText;
+    private GameObject scoreText;
+	// Use this for initialization
+	void Start () {
+        this.gameoverText = GameObject.Find("GameOverText");
+        this.scoreText = GameObject.Find("Score");
+        scoreText.GetComponent<Text>().text = "Score" + 0;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        
+		if(this.transform .position.z < this.visiblePosZ)
+        {
+            this.gameoverText.GetComponent<Text>().text = "Game Over";
+        }
+	}
+    private void OnCollisionEnter(Collision other)
+    {
+     if(other.gameObject .tag == "SmallStarTag")
+        {
+            score += 10;
+            scoreText.GetComponent<Text>().text = "Score" + score;
+        } 
+     if(other.gameObject .tag == "LargeStarTag")
+        {
+            score += 20;
+            scoreText.GetComponent<Text>().text = "Score" + score;
+        }
+     if(other.gameObject .name == "SmallCloudPrefab")
+        {
+            score += 30;
+            scoreText.GetComponent<Text>().text = "Score" + score;
+        }
+     if(other.gameObject .name == "LargeCloudPrefab")
+        {
+            score += 40;
+            scoreText.GetComponent<Text>().text = "Score" + score;
+        }
+    }
+}
---
+++ Assets/TouchController.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TouchController : MonoBehaviour {
+    private HingeJoint myHingeJoint;
+    float defaultAngle = 20f;
+    float flickAngle = -20f;
+    int width = Screen.width;
+	// Use this for initialization
+	void Start () {
+        this.myHingeJoint = GetComponent<HingeJoint>();
+        SetAngle(this.defaultAngle);
+		if(Input.touchSupported)
+        {
+           Debug .Log ("タッチに対応している");
+        }
+        else
+        {
+            Debug.Log("対応してない");
+        }
+	}
+
+    // Update is called once per frame
+    void Update() {
+        if(Input.touchCount > 0)
+        {
+            foreach (Touch t in Input.touches)
+            {
+                if (t.phase == TouchPhase.Moved && tag == "RightFripperTag" && t.position.x >= Screen.width / 2)
+                {
+                    Debug.Log("押してる");
+                    SetAngle(flickAngle);
+                }
+                if (t.phase == TouchPhase.Moved && tag == "LeftFripperTag" && t.position.x <= Screen.width / 2)
+                {
+                    Debug.Log("押してる");
+                    SetAngle(flickAngle);
+                }
+                if (t.phase == TouchPhase.Ended && tag == "RightFripperTag" && t.position.x >= Screen.width / 2)
+                {
+                    SetAngle(defaultAngle);
+                }
+                if (t.phase == TouchPhase.Ended  && tag == "LeftFripperTag" && t.position.x <= Screen.width / 2)
+                {
+                    SetAngle(defaultAngle);
+                }
+
+
+
+            }
+        }
+
+       
+	}
+    public void SetAngle(float angle)
+    {
+        JointSpring j = this.myHingeJoint.spring;
+        j.targetPosition = angle;
+        this.myHingeJoint.spring = j;
+    }
+}
